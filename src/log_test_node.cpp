f0ff888e84d697dcbec12b35c795ccf6edc05132
Sebastianhayashi <microseyuyu@outlook.jp>
2024-10-16 17:15:36 +0800

    update

--- src/log_test_node.cpp
+++ src/log_test_node.cpp
@@ -1,22 +1,33 @@
-#include <rclcpp/rclcpp.hpp>
+#include "rclcpp/rclcpp.hpp"
 
-class LogTestNode : public rclcpp::Node
+class LoggingTestNode : public rclcpp::Node
 {
 public:
-    LogTestNode() : Node("log_test_node")
-    {
-        RCLCPP_INFO(this->get_logger(), "Node started.");
-        RCLCPP_INFO(this->get_logger(), "This is an INFO log.");
-        RCLCPP_WARN(this->get_logger(), "This is a WARN log.");
-        RCLCPP_ERROR(this->get_logger(), "This is an ERROR log.");
-    }
+  LoggingTestNode() : Node("logging_test_node")
+  {
+    timer_ = this->create_wall_timer(
+      std::chrono::seconds(1),
+      std::bind(&LoggingTestNode::timer_callback, this));
+  }
+
+private:
+  void timer_callback()
+  {
+    RCLCPP_INFO(this->get_logger(), "Info message from %s", this->get_name());
+    RCLCPP_WARN(this->get_logger(), "Warning message from %s", this->get_name());
+    RCLCPP_ERROR(this->get_logger(), "Error message from %s", this->get_name());
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
 };
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<LogTestNode>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<LoggingTestNode>();
+  rclcpp::spin(node);
+
+  rclcpp::shutdown();
+  return 0;
 }
