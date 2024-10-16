7c2b17286114280e98387a5920e3086522de5510
Sebastianhayashi <microseyuyu@outlook.jp>
2024-10-16 16:43:58 +0800

    fix UnknownROSArgsError

--- launch/log_test_node_launch.py
+++ launch/log_test_node_launch.py
@@ -1,7 +1,8 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-import os
+from launch.actions import SetEnvironmentVariable
 
 def generate_launch_description():
     config_dir = os.path.join(
@@ -9,13 +10,14 @@ def generate_launch_description():
         'config', 'log4cxx.properties')
 
     return LaunchDescription([
+        # 设置环境变量来指定log4cxx配置文件
+        SetEnvironmentVariable('LOG4CXX_CONFIGURATION', config_dir),
+
         Node(
             package='my_ros2_logging_project',
             executable='log_test_node',
             name='log_test_node',
             output='log',
             parameters=[{'use_sim_time': False}],
-            remappings=[],
-            arguments=['--ros-args', '--log-config', config_dir],
-        )
+        ),
     ])
