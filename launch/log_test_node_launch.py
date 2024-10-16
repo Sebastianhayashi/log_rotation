c2d580f1bf2f3aabe91a1077e2f2acfcb25c2b78
Sebastianhayashi <microseyuyu@outlook.jp>
2024-10-16 17:44:47 +0800

    redesign

--- launch/log_test_node_launch.py
+++ launch/log_test_node_launch.py
@@ -1,22 +1,12 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-from launch.actions import SetEnvironmentVariable
 
 def generate_launch_description():
-    config_dir = os.path.join(
-        get_package_share_directory('my_ros2_logging_project'),
-        'config', 'log4cxx.properties')
-
     return LaunchDescription([
-        SetEnvironmentVariable('LOG4CXX_CONFIGURATION', config_dir),
-
         Node(
             package='my_ros2_logging_project',
             executable='log_test_node',
             name='log_test_node',
-            output='log',
-            parameters=[{'use_sim_time': False}],
-        ),
+            output='log'
+        )
     ])
--- src/log_test_node.cpp
+++ src/log_test_node.cpp
@@ -1,33 +1,47 @@
-#include "rclcpp/rclcpp.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/rotating_file_sink.h>  // 基于大小的日志轮转
+#include <spdlog/sinks/daily_file_sink.h>     // 基于时间的日志轮转
+#include <memory>
 
-class LoggingTestNode : public rclcpp::Node
+class LogTestNode : public rclcpp::Node
 {
 public:
-  LoggingTestNode() : Node("logging_test_node")
-  {
-    timer_ = this->create_wall_timer(
-      std::chrono::seconds(1),
-      std::bind(&LoggingTestNode::timer_callback, this));
-  }
+    LogTestNode() : Node("log_test_node")
+    {
+        // 创建基于大小的日志轮转器
+        auto rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+            "/home/user/.ros/log/my_node.log", 10485760, 5);  // 10MB, 保留5个文件
+        spdlog::logger rotating_logger("rotating_logger", rotating_sink);
 
-private:
-  void timer_callback()
-  {
-    RCLCPP_INFO(this->get_logger(), "Info message from %s", this->get_name());
-    RCLCPP_WARN(this->get_logger(), "Warning message from %s", this->get_name());
-    RCLCPP_ERROR(this->get_logger(), "Error message from %s", this->get_name());
-  }
+        // 创建基于时间的日志轮转器
+        auto daily_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(
+            "/home/user/.ros/log/my_node_daily.log", 0, 0);  // 每天创建新的日志文件
+        spdlog::logger daily_logger("daily_logger", daily_sink);
 
-  rclcpp::TimerBase::SharedPtr timer_;
+        // 输出不同级别的日志
+        rotating_logger.info("This is an INFO log message from log_test_node.");
+        rotating_logger.warn("This is a WARNING log message from log_test_node.");
+        rotating_logger.error("This is an ERROR log message from log_test_node.");
+
+        daily_logger.info("Daily log: INFO message");
+        daily_logger.warn("Daily log: WARNING message");
+        daily_logger.error("Daily log: ERROR message");
+    }
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
+    // 初始化ROS2
+    rclcpp::init(argc, argv);
 
-  auto node = std::make_shared<LoggingTestNode>();
-  rclcpp::spin(node);
+    // 创建节点
+    auto node = std::make_shared<LogTestNode>();
 
-  rclcpp::shutdown();
-  return 0;
+    // 运行节点
+    rclcpp::spin(node);
+
+    // 关闭ROS2
+    rclcpp::shutdown();
+    return 0;
 }
